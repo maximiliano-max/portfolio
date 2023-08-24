fc496aa4bec17c2ad7d683543e01458a1435b791
maximiliano <maximilianodonofio9@gmail.com>
2023-08-24 14:57:14 -0300

    2commit

--- menu.js
+++ menu.js
@@ -3,7 +3,7 @@ $(document).ready(main);
 var contador = 1;
 function main(){
 	$('.menu_bar').click(function(){
-		$('nav').toggle(); 
+		//$('nav').toggle(); 
 
 		if(contador == 1){
 			$('nav').animate({
